9c3b1546659b545e62c7975a5ed45c054d2ea7bf
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-08-19 01:13:46 -0300

    se agregó la configuracion para iconos

--- App.tsx
+++ App.tsx
@@ -2,15 +2,17 @@
 import {SafeAreaView} from 'react-native';
 //import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 import React from 'react';
-import {CounterScreen} from './src/presentation/screens/CounterScreen';
+//import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {PaperProvider} from 'react-native-paper';
+import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 export const App = () => {
   return (
     <PaperProvider>
       <SafeAreaView style={{flex: 1}}>
         {/* <HelloWorldScreen name="Fernando herrera" /> */}
-        <CounterScreen />
+        {/* <CounterScreen /> */}
+        <CounterM3Screen/>
       </SafeAreaView>
     </PaperProvider>
   );
