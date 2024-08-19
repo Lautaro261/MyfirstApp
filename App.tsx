5a7ee42741c3dba1a0b9a9a94f7779f575e6f37e
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-08-19 01:33:24 -0300

    finish

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-native/no-inline-styles */
 import {SafeAreaView} from 'react-native';
 //import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 import React from 'react';
 //import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {PaperProvider} from 'react-native-paper';
-import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
+import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 export const App = () => {
   return (
-    <PaperProvider>
+    <PaperProvider
+      settings={{
+        icon: props => <IonIcon {...props} />,
+      }}>
       <SafeAreaView style={{flex: 1}}>
         {/* <HelloWorldScreen name="Fernando herrera" /> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen/>
+        <CounterM3Screen />
       </SafeAreaView>
     </PaperProvider>
   );
--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {globalStyles} from '../themes/globalStyles';
 import {FAB} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/Ionicons';
+//import Icon from 'react-native-vector-icons/Ionicons';
 
 export const CounterM3Screen = () => {
   const [count, setCount] = useState(0);
@@ -10,13 +10,12 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
-      <Icon name="accessibility-outline" size={25} />
+      {/* <Icon name="accessibility-outline" size={25} /> */}
       <FAB
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-        label="+1"
         style={globalStyles.fab}
-        icon="add"
+        icon="add-outline"
       />
     </View>
   );
