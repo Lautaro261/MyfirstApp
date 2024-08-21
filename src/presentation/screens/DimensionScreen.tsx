bcbf43241d15f49f771a5d6d2118552cc246bb79
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-08-20 23:58:42 -0300

    Dimensions get y Hook: useWindowDimension

--- src/presentation/screens/DimensionScreen.tsx
+++ src/presentation/screens/DimensionScreen.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
+import {Text} from 'react-native-paper';
+//import {Dimensions} from 'react-native';
+
+//const dimensions = Dimensions.get('window');
+//const {width, height} = Dimensions.get('window'); No se actualiza al rotar el dispositivo
 
 export const DimensionScreen = () => {
+  const {width, height} = useWindowDimensions();
   return (
     <View>
-      <Text>DimensionScreen</Text>
+      <View style={styles.container}>
+        <View
+          style={{
+            ...styles.purpleBox,
+            width: width * 0.6,
+          }}
+        />
+      </View>
+
+      <Text style={styles.title}>
+        wedth: {width}, height: {height}
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 300,
+    backgroundColor: 'red',
+  },
+  purpleBox: {
+    backgroundColor: '#5856d6',
+    height: '50%',
+    width: '50%',
+  },
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+  },
+});
+// el Porcentaje  hace referencia al padre, es decir, tomara un porcentaje de las medidas del padre. En este caso Container.
